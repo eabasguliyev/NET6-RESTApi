1b83fbb8f6aff35d0578e0d49eb0d5a8d1b79589
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-12 21:04:55 +0400

    Persisting Entities with MongoDB

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,14 +1,25 @@
 using Catalog.Repositories;
+using Catalog.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
+builder.Services.AddSingleton<IMongoClient>(serviceProvider => {
+    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    return new MongoClient(settings.ConnectionString);
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
 
 var app = builder.Build();
 
